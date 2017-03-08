9583601e17c2869f26b38e5d25ac72899001185a
toyama0919 <toyama0919@gmail.com>
2017-03-08 18:19:19 +0900

    fix thor option to symbol.

--- lib/ej/commands.rb
+++ lib/ej/commands.rb
@@ -25,7 +25,7 @@ module Ej
     def initialize(args = [], options = {}, config = {})
       super(args, options, config)
       @global_options = config[:shell].base.options
-      @core = Ej::Core.new(@global_options['host'], @global_options['index'], @global_options['debug'])
+      @core = Ej::Core.new(@global_options[:host], @global_options[:index], @global_options[:debug])
     end
 
     desc '-s [lucene query]', 'search'
@@ -36,30 +36,30 @@ module Ej
     option :source_only, type: :boolean, aliases: '--so', default: true, desc: 'from'
     option :query, type: :string, aliases: '-q', default: nil, desc: 'query'
     option :sort, type: :hash, aliases: '--sort', default: nil, desc: 'ex. --sort @timestamp:desc'
-    def search(query = options['query'])
-      puts_with_format(@core.search(options['type'],
+    def search(query = options[:query])
+      puts_with_format(@core.search(options[:type],
                              query,
-                             options['size'],
-                             options['from'],
-                             options['source_only'],
+                             options[:size],
+                             options[:from],
+                             options[:source_only],
                              nil,
-                             options['fields'],
-                             options['sort']
+                             options[:fields],
+                             options[:sort]
                              ))
     end
 
     desc 'total [lucene query]', 'total'
     option :type, type: :string, aliases: '-t', default: nil, desc: 'type'
     option :query, type: :string, aliases: '-q', default: nil, desc: 'query'
-    def count(query = options['query'])
-      puts_with_format(@core.search(options['type'], query, 0, 0, false))
+    def count(query = options[:query])
+      puts_with_format(@core.search(options[:type], query, 0, 0, false))
     end
 
     desc 'distinct [lucene query]', 'distinct'
     option :type, type: :string, aliases: '-t', default: nil, desc: 'type'
     option :query, type: :string, aliases: '-q', default: nil, desc: 'query'
     def distinct(term)
-      puts_with_format(@core.distinct(term, options['type'], options['query']))
+      puts_with_format(@core.distinct(term, options[:type], options[:query]))
     end
 
     desc 'copy', 'copy index'
@@ -71,12 +71,12 @@ module Ej
     option :from, type: :numeric, default: 0, desc: 'from'
     def copy
       @core.copy(
-        options['source'],
-        options['dest'],
-        options['query'],
-        options['per'],
-        options['proc_num'],
-        options['from']
+        options[:source],
+        options[:dest],
+        options[:query],
+        options[:per],
+        options[:proc_num],
+        options[:from]
       )
     end
 
@@ -84,14 +84,14 @@ module Ej
     option :query, type: :string, aliases: '-q', default: nil, desc: 'query'
     option :per, type: :numeric, default: nil, desc: 'per'
     def dump
-      @core.dump(options['query'], options['per'])
+      @core.dump(options[:query], options[:per])
     end
 
     desc '-f', 'facet'
     option :query, type: :string, aliases: '-q', default: '*', desc: 'query'
     option :size, type: :numeric, aliases: '-n', default: 10, desc: 'size'
     def facet(term)
-      puts_with_format(@core.facet(term, options['size'], options['query']))
+      puts_with_format(@core.facet(term, options[:size], options[:query]))
     end
 
     desc 'aggs', 'aggs'
@@ -99,19 +99,19 @@ module Ej
     option :size, type: :numeric, aliases: '-n', default: 10, desc: 'size'
     option :terms, type: :array, aliases: '-t', required: true, desc: 'size'
     def aggs
-      puts_with_format(@core.aggs(options['terms'], options['size'], options['query']))
+      puts_with_format(@core.aggs(options[:terms], options[:size], options[:query]))
     end
 
     desc 'min', 'term'
     option :term, type: :string, aliases: '-k', desc: 'terms'
     def min
-      puts_with_format(@core.min(options['term']))
+      puts_with_format(@core.min(options[:term]))
     end
 
     desc 'max', 'count record, group by keys'
     option :term, type: :string, aliases: '-k', desc: 'terms'
     def max
-      puts_with_format(@core.max(options['term']))
+      puts_with_format(@core.max(options[:term]))
     end
 
     desc '-b', 'bulk import STDIN JSON'
@@ -121,7 +121,7 @@ module Ej
     option :add_timestamp, type: :boolean, default: true, desc: 'add_timestamp'
     option :id_keys, type: :array, aliases: '--id', default: nil, desc: 'id'
     def bulk
-      @core.bulk(options['timestamp_key'], options['type'], options['add_timestamp'], options['id_keys'], options['index'])
+      @core.bulk(options[:timestamp_key], options[:type], options[:add_timestamp], options[:id_keys], options[:index])
     end
 
     desc 'health', 'health'
@@ -166,8 +166,8 @@ module Ej
     option :type, aliases: '-t', type: :string, default: nil, required: true, desc: 'type'
     option :path, type: :string, default: nil, required: true, desc: 'path'
     def put_routing
-      body = { options['type'] => {"_routing"=>{"required"=>true, "path"=>options['path']}}}
-      puts_with_format(@core.put_mapping(options['index'], options['type'], body))
+      body = { options[:type] => {"_routing"=>{"required"=>true, "path"=>options[:path]}}}
+      puts_with_format(@core.put_mapping(options[:index], options[:type], body))
     end
 
     desc 'put_template', 'put template'
@@ -180,7 +180,7 @@ module Ej
     option :alias, type: :string, aliases: '-a', default: nil, required: true, desc: 'alias name'
     option :indices, type: :array, aliases: '-x', default: nil, required: true, desc: 'index array'
     def create_aliases
-      @core.create_aliases(options['alias'], options['indices'])
+      @core.create_aliases(options[:alias], options[:indices])
     end
 
     desc 'recovery', 'recovery'
@@ -193,13 +193,13 @@ module Ej
     option :type, type: :string, aliases: '-t', default: nil, desc: 'type'
     option :query, type: :string, aliases: '-q', default: nil, desc: 'query'
     def delete
-      @core.delete(options['index'], options['type'], options['query'])
+      @core.delete(options[:index], options[:type], options[:query])
     end
 
     desc 'delete_template --name [name]', 'delete_template'
     option :name, type: :string, default: nil, required: true, desc: 'template name'
     def delete_template
-      @core.delete_template(options['name'])
+      @core.delete_template(options[:name])
     end
 
     desc 'template', 'show template'
