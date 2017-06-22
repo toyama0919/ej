c88e4350b9deac6ab7874f94bdeba35cd9a91977
Hiroshi Toyama <toyama-h@ipros.jp>
2017-06-22 19:22:04 +0900

    use faraday client.

--- lib/ej/values.rb
+++ lib/ej/values.rb
@@ -7,12 +7,26 @@ module Ej
     def initialize(global_options)
       @logger =  Logger.new($stderr)
       @logger.level = global_options[:debug] ? Logger::DEBUG : Logger::INFO
-      @client = Elasticsearch::Client.new(
-        hosts: global_options[:host],
-        logger: @logger,
-        index: global_options[:index]
-      )
+      @client = get_client(global_options[:host], global_options[:index])
       @index = global_options[:index]
+    end
+
+    def get_client(host, index)
+      host, port = host.split(':')
+      transport = ::Elasticsearch::Transport::Transport::HTTP::Faraday.new(
+        {
+          hosts: [{ host: host, port: port }],
+          options: {
+            reload_connections: true,
+            reload_on_failure: false,
+            retry_on_failure: 5,
+            transport_options: {
+              request: { timeout: 300 }
+            }
+          }
+        }
+      )
+      ::Elasticsearch::Client.new transport: transport, index: index, logger: @logger
     end
   end
 end
